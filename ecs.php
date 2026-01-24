65ffdab34fa4bfbd2667f3bde681d51ae3193833
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2026-01-24 15:47:48 -0300

    feat(dev): Add `php-forge/coding-standard` to development dependencies for code quality checks (#119)

--- ecs.php
+++ ecs.php
@@ -2,94 +2,12 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ClassNotation\{ClassDefinitionFixer, OrderedClassElementsFixer, OrderedTraitsFixer};
-use PhpCsFixer\Fixer\Import\{NoUnusedImportsFixer, OrderedImportsFixer};
-use PhpCsFixer\Fixer\LanguageConstruct\NullableTypeDeclarationFixer;
-use PhpCsFixer\Fixer\Phpdoc\PhpdocTypesOrderFixer;
-use PhpCsFixer\Fixer\PhpUnit\PhpUnitTestCaseStaticMethodCallsFixer;
-use PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer;
-use Symplify\EasyCodingStandard\Config\ECSConfig;
+/** @var \Symplify\EasyCodingStandard\Configuration\ECSConfigBuilder $ecsConfigBuilder */
+$ecsConfigBuilder = require __DIR__ . '/vendor/php-forge/coding-standard/config/ecs.php';
 
-return ECSConfig::configure()
-    ->withConfiguredRule(
-        ClassDefinitionFixer::class,
-        [
-            'space_before_parenthesis' => true,
-        ],
-    )
-    ->withConfiguredRule(
-        NullableTypeDeclarationFixer::class,
-        [
-            'syntax' => 'union',
-        ],
-    )
-    ->withConfiguredRule(
-        OrderedClassElementsFixer::class,
-        [
-            'order' => [
-                'use_trait',
-                'constant_public',
-                'constant_protected',
-                'constant_private',
-                'case',
-                'property_public',
-                'property_protected',
-                'property_private',
-                'construct',
-                'destruct',
-                'magic',
-                'method_public_abstract',
-                'method_protected_abstract',
-                'method_public',
-                'method_protected',
-                'method_private',
-            ],
-            'sort_algorithm' => 'alpha',
-        ],
-    )
-    ->withConfiguredRule(
-        OrderedImportsFixer::class,
-        [
-            'imports_order' => [
-                'class',
-                'function',
-                'const',
-            ],
-            'sort_algorithm' => 'alpha',
-        ],
-    )
-    ->withConfiguredRule(
-        PhpdocTypesOrderFixer::class,
-        [
-            'sort_algorithm' => 'none',
-            'null_adjustment' => 'always_last',
-        ],
-    )
-    ->withConfiguredRule(
-        PhpUnitTestCaseStaticMethodCallsFixer::class,
-        [
-            'call_type' => 'self',
-        ],
-    )
-    ->withFileExtensions(['php'])
-    ->withPaths(
-        [
-            __DIR__ . '/src',
-            __DIR__ . '/tests',
-        ],
-    )
-    ->withPhpCsFixerSets(perCS30: true)
-    ->withPreparedSets(
-        cleanCode: true,
-        comments: true,
-        docblocks: true,
-        namespaces: true,
-        strict: true,
-    )
-    ->withRules(
-        [
-            NoUnusedImportsFixer::class,
-            OrderedTraitsFixer::class,
-            SingleQuoteFixer::class,
-        ]
-    );
+return $ecsConfigBuilder->withPaths(
+    [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ],
+);
